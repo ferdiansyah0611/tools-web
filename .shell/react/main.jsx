b96a34d4c170917703d6d5bf50e6c76880ddb9d8
Ferdiansyah <ferdif9996@gmail.com>
2022-05-09 16:28:49 +0700

    upgrade react render v18

--- .shell/react/main.jsx
+++ .shell/react/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-  	<Provider store={store}>
-    	<App />
-  	</Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
